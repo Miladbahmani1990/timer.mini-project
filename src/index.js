c3e53372ee15fff548107aeba3a18e53dece0ea7
milad <soushiyanet.1990@gmail.com>
2022-01-26 09:39:25 +0330

    component calss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import react from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -24,4 +25,55 @@ ReactDOM.render(eleme,document.getElementById('root'));
 
 setInterval(() => {
   tick();
+}, 1000);
+
+
+
+//////////////////// component class ////////////////
+
+class Timer extends React.Component{
+
+  render(){
+
+    return(
+      <h3>
+          it is {new Date().toLocaleTimeString()}
+      </h3>
+    )
+  }
+}
+
+class Word extends React.Component{
+  render(){
+    return(
+      <h1>
+          timer by class component
+      </h1>
+    )
+  }
+}
+
+class App extends React.Component{
+
+  render(){
+    return (
+
+      <div>
+
+        
+        <Word/>
+        <Timer/>
+        
+  
+      </div>
+    )
+  }
+}
+
+const mytick=()=>{
+ReactDOM.render(<App/>,document.getElementById("mydiv"));
+}
+
+setInterval(() => {
+  mytick();
 }, 1000);
