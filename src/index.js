e313408104cf7e83de5e0e8960ff86037e7f9618
milad <soushiyanet.1990@gmail.com>
2022-01-26 13:09:23 +0330

    state

--- src/index.js
+++ src/index.js
@@ -77,3 +77,66 @@ ReactDOM.render(<App/>,document.getElementById("mydiv"));
 setInterval(() => {
   mytick();
 }, 1000);
+
+
+//////////// state ///////////////////
+ //  برای نمایش تغییرات مقادیر در صفحه 
+
+class Timer1 extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      time: new Date().toLocaleTimeString()
+    }
+  }
+  render(){
+    setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString()
+      })
+    }, 1000);
+
+    return(
+      <h3>
+          it is {this.state.time}
+      </h3>
+    )
+  }
+}
+
+class Word1 extends React.Component{
+  render(){
+    return(
+      <h1>
+          timer by state
+      </h1>
+    )
+  }
+}
+
+class App1 extends React.Component{
+
+  render(){
+    return (
+
+      <div>
+
+        
+        <Word1/>
+        <Timer1/>
+        
+  
+      </div>
+    )
+  }
+}
+
+// const mytick=()=>{
+
+ReactDOM.render(<App1/>,document.getElementById("mydiv1"));
+
+//}
+
+// setInterval(() => {
+//   mytick();
+// }, 1000);
